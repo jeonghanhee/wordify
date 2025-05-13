6cecfe6cca3bcd0862b1bee212a1d8b3ba207b75
origin <https://github.com/tjertmkr/gptcat.git>
2025-05-13 12:18:06 +0900

    Add setFormat, toFormat

--- test.ts
+++ test.ts
@@ -1,3 +1,15 @@
 import "./wordify";
+import { setFormat } from "./wordify";
 
-console.log("helloWorld".toPretty())
+console.log("helloWorld".toPretty());
+
+console.log("USER LOGIN LOG".toCamelCase())
+console.log("USER LOGIN LOG".toPascalCase())
+console.log("USER LOGIN LOG".toKebabCase())
+console.log("USER LOGIN LOG".toSnakeCase())
+
+setFormat((text: string) => {
+    return text.toUpperCase();
+})
+
+console.log("helloworld".toFormat());
--- wordify.ts
+++ wordify.ts
@@ -1,8 +1,15 @@
-export {}
+export {
+    setFormat
+}
 
 declare global {
     interface String {
       toPretty(this: string): string;
+      toCamelCase(this: string): string;
+      toPascalCase(this: string): string;
+      toKebabCase(this: string): string;
+      toSnakeCase(this: string): string;
+      toFormat(this: string): string;
     }
 }
 
@@ -11,8 +18,9 @@ declare global {
  * Makes the string pretty.
  * @constructor
  * @param {string} this 
+ * @returns
  */
-const toPretty = function (this: string) {
+const toPretty = function (this: string): string {
     var new_text: string = "";
     let char: string = "";
 
@@ -35,5 +43,86 @@ const toPretty = function (this: string) {
     return new_text;
 };
 
+/**
+ * Convert string to CamelCase
+ * @constructor
+ * @param {string} this
+ * @returns
+ */
+const toCamelCase = function (this: string): string {
+    var new_text = "";
+
+    this.split(" ").forEach(text => {
+        new_text += text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+    })
+    return new_text.charAt(0).toLowerCase() + new_text.slice(1);
+}
+
+/**
+ * Convert string to PascalCase
+ * @constructor
+ * @param {string} this
+ * @returns 
+ */
+const toPascalCase = function (this: string): string {
+    var new_text = this.toCamelCase();
+    return new_text.charAt(0).toUpperCase() + new_text.slice(1);
+}
+
+/**
+ * Convert string to KebabCase
+ * @constructor
+ * @param {string} this
+ * @returns
+ */
+const toKebabCase = function (this: string): string {
+    return this.replace(/ /gi, "-").toLowerCase();
+}
+
+/**
+ * Convert string to SnakeCase
+ * @constructor
+ * @param {string} this
+ * @returns
+ */
+const toSnakeCase = function (this: string): string {
+    return this.replace(/ /gi, "_").toLowerCase();
+}
+
+// static area
+type FormatFunc = (text: string) => string;
+class StaticFormatter {
+    private static instance: StaticFormatter;
+    private func: FormatFunc;
+
+    constructor() {
+    }
+
+    static getInstance(): StaticFormatter {
+        if (!this.instance)
+            this.instance = new StaticFormatter();
+
+        return this.instance;
+    }
+
+    setFunc(func: FormatFunc) {
+        this.func = func;
+    }
+
+    getFunc() {
+        return this.func;
+    }
+}
+const setFormat = (func: FormatFunc) => StaticFormatter.getInstance().setFunc(func);
+
+const toFormat = function (this: string): string {
+    return StaticFormatter.getInstance().getFunc()(this);
+}
+
 // noImplicitThis  
 String.prototype.toPretty = toPretty;
+String.prototype.toCamelCase = toCamelCase;
+String.prototype.toPascalCase = toPascalCase;
+String.prototype.toKebabCase = toKebabCase;
+String.prototype.toSnakeCase = toSnakeCase;
+String.prototype.toFormat = toFormat;
